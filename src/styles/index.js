275a0ab48295fc290589088dcf042c645bf8a98a
Thalles Albert <thalles.0423@gmail.com>
2020-02-08 21:40:33 -0300

    Initial Commit

---
+++ src/styles/index.js
@@ -0,0 +1,20 @@
+import {StyleSheet} from 'react-native'
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#0066cc',
+      justifyContent: 'center',
+      alignItems: 'center'
+    },
+    texto: {
+      color: '#fff',
+      fontSize: 22
+    },
+    slogan: {
+      color: '#000',
+      fontSize: 18
+    }
+  })
+
+export default styles
